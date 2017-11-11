8d6959168f1c94e6522abb91da1839a60c60c8b6
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-11 18:44:14 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Build packages
+for i in $(ls -d src/*/); do
+  helm package \
+    -d dist \
+    ${i%%/}
+done
+
+# Update repo index
+helm repo index dist \
+  --url https://ukube.github.com/charts/dist
