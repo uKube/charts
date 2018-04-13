56ff2824b7b0c64e3a47e5e1e87f007bf0ffbe32
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2018-04-13 09:20:18 +0200

    Add support for charts that may have dependencies

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 
 # Build packages
 for i in $(ls -d src/*/); do
+  # Pull dependencies only if requirements.yaml is present
+  if [ -f "${i%%/}/requirements.yaml" ]; then
+    rm -f ${i%%/}/requirements.lock 2>&1 >&/dev/null
+    rm -rf ${i%%/}/charts/ 2>&1 >&/dev/null
+
+    helm dep build \
+      ${i%%/}
+  fi
+
+  # Build the chart
   helm package \
     -d dist \
     ${i%%/}
